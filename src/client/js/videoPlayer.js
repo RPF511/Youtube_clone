2e66bf57f8eb184705036a58dee5ebff1acf0a8f
RPF511 <sbk98726@gmail.com>
2021-10-02 11:15:51 +0900

    simple video player command

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,57 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5
+video.volume = volumeValue;
+
+
+const handlePlay = (e) => {
+    if(video.paused){
+        video.play();
+    }
+    else{
+        video.pause()
+    }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
+}
+
+const handleMute = (e) => {
+    if(video.muted){
+        video.muted = false;
+    }
+    else{
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+}
+
+const handleVolumeRange =(e) => {
+    const {
+        target: {value},
+    } = event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "mute";
+    }
+    volumeValue = value
+    video.volume = value;
+}
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+}
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeRange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/client/js/main.js",
+    entry: {
+        main: "./src/client/js/main.js",
+        videoPlayer: "./src/client/js/videoPlayer.js"
+    },
     mode: 'development',
     watch: true,
     plugins: [new MiniCssExtractPlugin({
@@ -12,7 +15,7 @@ module.exports = {
     
     output: {
         //to make js file in assets/js folder
-        filename: "js/main.js",
+        filename: "js/[name].js",
         path: path.resolve(__dirname, "assets"),
         //clean output folder before build
         clean: true,
