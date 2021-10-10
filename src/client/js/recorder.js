6dfc887c1afb3a417d92f02e0e84f53814ea042a
RPF511 <sbk98726@gmail.com>
2021-10-10 10:19:31 +0900

    recorder in progress

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async() => {
     const stream = await navigator.mediaDevices.getUserMedia({
         audio:true, 
-        video:true
+        video:true,
+        // video:{width:200,height:100};
     });
-    console.log(stream);
+    video.srcObject = stream;
+    video.onplay();
 };
 
 startBtn.addEventListener("click", handleStart);
