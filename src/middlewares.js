3748e7a67ec8e2d78cd14cec54163db00603486a
RPF511 <sbk98726@gmail.com>
2022-08-14 13:10:11 +0900

    aws

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,20 @@
 //Session data is not saved in the cookie itself, just the session ID. Session data is stored server-side.
 
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET
+    }
+});
+
+const multerUPloader = multerS3({
+    s3: s3,
+    bucket: 'youtube-clone-rpf511',
+});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Youtube_C"
@@ -29,15 +43,19 @@ export const publicOnlyMiddleware = (req,res,next) =>{
     }
 };
 
+
+
 export const avatarUpload = multer({ 
     dest:"uploads/avatars/",
     limits:{
         fileSize: 3000000,
     },
+    storage : multerUPloader,
 });
 export const videoUpload = multer({ 
     dest:"uploads/videos/", 
     limits:{
         fileSize: 100000000,
     },
+    storage : multerUPloader,
 });
